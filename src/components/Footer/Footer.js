0a2fc855cfc443e2217c06ccfd57a9350d908553
allistermugaisi <allistermugaisi@gmail.com>
2022-02-23 19:44:52 +0300

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HeartTwoTone } from '@ant-design/icons';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			Mind Mantra Chat App <HeartTwoTone twoToneColor="#eb2f96" />
+		</footer>
+	);
+};
+
+export default Footer;
